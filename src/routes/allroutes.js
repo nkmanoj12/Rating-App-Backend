b2eafa4a1eef69558852c6f5d99b7b047227f038
nkmanoj12 <manu12.work@gmail.com>
2022-10-06 17:43:21 +0530

    usernameCheck API created

--- src/routes/allroutes.js
+++ src/routes/allroutes.js
@@ -2,16 +2,21 @@ const express  = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
 const signup = require("./signup");
 const login = require("./login");
 const updateProfile = require("./updateProfile");
 const resetPassword = require("./resetPassword");
+const usernameCheck = require("./usernameCheck");
 const temp = require("./temp");
 
 app.use("/", signup);
 app.use("/", login);
 app.use("/", updateProfile);
 app.use("/", resetPassword);
+app.use("/", usernameCheck);
 app.use("/", temp);
 
 module.exports = app;
---
+++ src/routes/usernameCheck.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = new express.Router();
+
+router.get("/usernameCheck", async function(req, res) {
+    if(req.body.username === null || req.body.username === undefined) {
+        res.send({statusCode : 400, statusMessage : "Failure", message : "Username required"});
+        return false;
+    }
+
+    const dataCount = await db.collection("users").find({username : req.body.username}).count();
+    if(dataCount != 0) {
+        res.send({statusCode : 400, statusMessage : "Failure", message : "Username not available"});
+        return false;
+    }
+
+    res.send({statusCode : 200, statusMessage : "Success", message : "Username available"});
+})
+
+module.exports = router;
